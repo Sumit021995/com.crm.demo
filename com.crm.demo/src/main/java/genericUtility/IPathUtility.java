e2a5380d6eb7d0b3f3bf73ce76f7b65f12ab08a6
Sumit021995 <kaku.saurav@gmail.com>
2025-01-09 15:14:42 +0530

    new Commit

--- com.crm.demo/src/main/java/genericUtility/IPathUtility.java
+++ com.crm.demo/src/main/java/genericUtility/IPathUtility.java
@@ -2,6 +2,6 @@ package genericUtility;
 
 public interface IPathUtility {
 	final String propertiesPath = ".\\src\\test\\resources\\TestData\\TestData.properties";
-	final String excelFilePath = ".\\src\\test\\resources\\TestData\\crmExcel.xlsx";
+	final String excelFilePath = ".\\src\\test\\resources\\TestData\\TestExcel.xlsx";
 	final String moduleExcelFilePath = ".\\src\\test\\resources\\TestData\\Module.xlsx";
 }
