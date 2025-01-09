7d2176ae782b221c497868f61909f21b5cb67371
Sumit021995 <kaku.saurav@gmail.com>
2025-01-09 12:41:57 +0530

    commit javaUtilityClass

--- com.crm.demo/src/main/java/genericUtility/JavaUtility.java
+++ com.crm.demo/src/main/java/genericUtility/JavaUtility.java
@@ -1,5 +1,29 @@
 package genericUtility;
 
-public class JavaUtility {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
+public class JavaUtility {
+	/**
+	 * This is a generic method to generate random number
+	 * @param bound
+	 * @return
+	 */
+	public int  generateRandomNumber(int bound)
+	{
+		Random r = new Random();
+		int num = r.nextInt(bound);
+		return num;
+	}
+	public String  getCalanderDetails(String format)
+	{
+		Calendar cal = Calendar.getInstance();
+		Date d = cal.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		String data = sdf.format(d);
+		return data;
+		
+	}
 }
