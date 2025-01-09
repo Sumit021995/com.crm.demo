8e0d4277ff4da8e9cbdb3e37c433b5e332137f48
Sumit021995 <kaku.saurav@gmail.com>
2025-01-09 17:17:54 +0530

    new commit

--- com.crm.demo/src/main/java/objectRepo/CreateLeadsPage.java
+++ com.crm.demo/src/main/java/objectRepo/CreateLeadsPage.java
@@ -6,36 +6,65 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
-public class CreateLeadsPage {
+public class CreateLeadsPage 
+{
+	@FindBy(xpath="//select[@name='salutationtype']") private WebElement titleDropDown;
 	@FindBy(xpath="//input[@name='firstname']") private WebElement firstNameTextField;
 	@FindBy(xpath="//input[@name='lastname']") private WebElement lastNameTextField;
 	@FindBy(xpath="//input[@name='company']") private WebElement companyTextField;
-	@FindBy(id="email") private WebElement emailTextField;
-	@FindBy(xpath="//input[@name='firstname']") private WebElement titleTextField;
-	@FindBy(xpath="//input[@name='mailingcity']") private WebElement mailingCityTextField;
-	@FindBy(xpath="//input[@name='mailingstate']") private WebElement mailingStateTextField;
+	@FindBy(id="designation") private WebElement titleTextField;
+	@FindBy(id="phone") private WebElement phoneNumTextField;
+	@FindBy(id="city") private WebElement cityTextField;
+	@FindBy(id="state") private WebElement stateTextField;
+	@FindBy(id="country") private WebElement countryTextField;
+	@FindBy(xpath="//select[@name='leadsource']") private WebElement leadSourceDropDown;
+	@FindBy(xpath="//select[@name='website']") private WebElement websiteTextField;
+	@FindBy(xpath="//input[@name='industry']") private WebElement industryDropDown;
+	@FindBy(xpath="//input[@name='annualrevenue']") private WebElement annualrevenueNumTextField;
+	@FindBy(xpath="//input[@name='noofemployees']") private WebElement noOfEmployeesTextField;
 	@FindBy(xpath="//input[@class='crmButton small save']") private WebElement saveBtn;
 	
+	public WebElement getTitleDropDown() {
+		return titleDropDown;
+	}
 	public WebElement getFirstNameTextField() {
 		return firstNameTextField;
 	}
 	public WebElement getLastNameTextField() {
 		return lastNameTextField;
 	}
-	public WebElement getLeadsourceDropdown() {
+	public WebElement getCompanyTextField() {
 		return companyTextField;
-	}
-	public WebElement getEmailTextField() {
-		return emailTextField;
 	}
 	public WebElement getTitleTextField() {
 		return titleTextField;
 	}
-	public WebElement getMailingCityTextField() {
-		return mailingCityTextField;
+	public WebElement getPhoneNumTextField() {
+		return phoneNumTextField;
 	}
-	public WebElement getMailingStateTextField() {
-		return mailingStateTextField;
+	public WebElement getCityTextField() {
+		return cityTextField;
+	}
+	public WebElement getStateTextField() {
+		return stateTextField;
+	}
+	public WebElement getCountryTextField() {
+		return countryTextField;
+	}
+	public WebElement getLeadSourceDropDown() {
+		return leadSourceDropDown;
+	}
+	public WebElement getWebsiteTextField() {
+		return websiteTextField;
+	}
+	public WebElement getIndustryDropDown() {
+		return industryDropDown;
+	}
+	public WebElement getAnnualrevenueNumTextField() {
+		return annualrevenueNumTextField;
+	}
+	public WebElement getNoOfEmployeesTextField() {
+		return noOfEmployeesTextField;
 	}
 	public WebElement getSaveBtn() {
 		return saveBtn;
@@ -43,33 +72,33 @@ public class CreateLeadsPage {
 	public CreateLeadsPage(WebDriver driver) {
 		PageFactory.initElements(driver,this);
 	}
-	public void createContactOperation(String firstName,String lastName,String leadSourceValue)
+	public void createContactOperation(String firstName,String lastName,String company)
 	{
 		getFirstNameTextField().sendKeys(firstName);
 		getLastNameTextField().sendKeys(lastName);
-		Select s = new Select(getLeadsourceDropdown());
-		s.selectByValue(leadSourceValue);
+		getCompanyTextField().sendKeys(company);
 		getSaveBtn().click();
 	}
 	
-	public void createContactOperation(String firstName,String lastName,String title , String email)
+	public void createContactOperation(String firstName,String lastName,String phone , String website)
 	{
 		getFirstNameTextField().sendKeys(firstName);
 		getLastNameTextField().sendKeys(lastName);
-		getEmailTextField().sendKeys(email);
-		getTitleTextField().sendKeys(title);
+		getPhoneNumTextField().sendKeys(phone);
+		getWebsiteTextField().sendKeys(website);
 		getSaveBtn().click();
 	}
-	public void createContactOperation(String firstName,String lastName,String title,String email , String city,String state)
+	public void createContactOperation(String firstName,String lastName,String phone,String website,String noOfEmployee,String country,String city,String state)
 	{
 		getFirstNameTextField().sendKeys(firstName);
 		getLastNameTextField().sendKeys(lastName);
-		getEmailTextField().sendKeys(email);
-		getMailingCityTextField().sendKeys(city);
-		getMailingStateTextField().sendKeys(state);
+		getPhoneNumTextField().sendKeys(phone);
+		getWebsiteTextField().sendKeys(website);
+		getNoOfEmployeesTextField().sendKeys(noOfEmployee);
+		getCountryTextField().sendKeys(country);
+		getCityTextField().sendKeys(city);
+		getStateTextField().sendKeys(state);
 		getSaveBtn().click();
 	}
-	
-	
 }
 
