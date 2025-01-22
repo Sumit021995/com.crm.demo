1af17588d597ddc76185fc38c4a2bed6b24087cf
Sumit021995 <kaku.saurav@gmail.com>
2025-01-23 01:13:13 +0530

    new commit

--- com.crm.demo/src/test/java/objectRepo/TC_003.java
+++ com.crm.demo/src/test/java/objectRepo/TC_003.java
@@ -13,24 +13,24 @@ public class TC_003 extends BaseClass
 	{
 		System.out.println("Test Execution started");
 		HomePage hp=new HomePage(driver);
-		hp.clickOnLeadsMenu();
+		hp.clickOnLeadMenuBtn();
 		
 		LeadsPage lp=new LeadsPage(driver);
-		lp.clickOnLeadsContactButton();
+		lp.clickOnCreateLeadsPlusIcon();
 		
 		ExcelUtility eUtil=new ExcelUtility();
-		String fName=eUtil.getDataFromExcel("Leads", 8, 1);
-		String lName=eUtil.getDataFromExcel("Leads", 8, 2);
-		String company=eUtil.getDataFromExcel("Leads", 8, 3);
-		String phone=eUtil.getDataFromExcel("Leads", 8, 4);
-		String website=eUtil.getDataFromExcel("Leads", 8, 5);
-		String noOfEmployees=eUtil.getDataFromExcel("Leads", 8, 6);
-		String country=eUtil.getDataFromExcel("Leads", 8, 7);
-		String city=eUtil.getDataFromExcel("Leads", 8, 8);
-		String state=eUtil.getDataFromExcel("Leads", 8, 9);
+		String fName=eUtil.fetchingDataFromExcelFile("Leads", 8, 1);
+		String lName=eUtil.fetchingDataFromExcelFile("Leads", 8, 2);
+		String company=eUtil.fetchingDataFromExcelFile("Leads", 8, 3);
+		String phone=eUtil.fetchingDataFromExcelFile("Leads", 8, 4);
+		String website=eUtil.fetchingDataFromExcelFile("Leads", 8, 5);
+		String noOfEmployees=eUtil.fetchingDataFromExcelFile("Leads", 8, 6);
+		String country=eUtil.fetchingDataFromExcelFile("Leads", 8, 7);
+		String city=eUtil.fetchingDataFromExcelFile("Leads", 8, 8);
+		String state=eUtil.fetchingDataFromExcelFile("Leads", 8, 9);
 	
-		CreatingNewLeadPage cnlp=new CreatingNewLeadPage(driver);
-		cnlp.createNewLead(fName, lName, company, phone, website, noOfEmployees, country, city, state);
+		CreateLeadsPage cnlp=new CreateLeadsPage(driver);
+		cnlp.createLeadOperation(fName, lName, phone, website);
 		System.out.println("Test Execution Ended");
 	}
 
