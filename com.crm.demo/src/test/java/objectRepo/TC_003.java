5791153560cfe5ab8adff86a51df9dd650c32a18
Sumit021995 <kaku.saurav@gmail.com>
2025-01-23 01:13:46 +0530

    new commit

--- com.crm.demo/src/test/java/objectRepo/TC_003.java
+++ com.crm.demo/src/test/java/objectRepo/TC_003.java
@@ -21,13 +21,13 @@ public class TC_003 extends BaseClass
 		ExcelUtility eUtil=new ExcelUtility();
 		String fName=eUtil.fetchingDataFromExcelFile("Leads", 8, 1);
 		String lName=eUtil.fetchingDataFromExcelFile("Leads", 8, 2);
-		String company=eUtil.fetchingDataFromExcelFile("Leads", 8, 3);
+//		String company=eUtil.fetchingDataFromExcelFile("Leads", 8, 3);
 		String phone=eUtil.fetchingDataFromExcelFile("Leads", 8, 4);
 		String website=eUtil.fetchingDataFromExcelFile("Leads", 8, 5);
-		String noOfEmployees=eUtil.fetchingDataFromExcelFile("Leads", 8, 6);
-		String country=eUtil.fetchingDataFromExcelFile("Leads", 8, 7);
-		String city=eUtil.fetchingDataFromExcelFile("Leads", 8, 8);
-		String state=eUtil.fetchingDataFromExcelFile("Leads", 8, 9);
+//		String noOfEmployees=eUtil.fetchingDataFromExcelFile("Leads", 8, 6);
+//		String country=eUtil.fetchingDataFromExcelFile("Leads", 8, 7);
+//		String city=eUtil.fetchingDataFromExcelFile("Leads", 8, 8);
+//		String state=eUtil.fetchingDataFromExcelFile("Leads", 8, 9);
 	
 		CreateLeadsPage cnlp=new CreateLeadsPage(driver);
 		cnlp.createLeadOperation(fName, lName, phone, website);
