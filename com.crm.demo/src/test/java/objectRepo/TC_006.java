2eb6b65cc077ab000a56f93c0793a1884e64ce3a
Sumit021995 <kaku.saurav@gmail.com>
2025-01-23 11:42:58 +0530

    new commit

--- com.crm.demo/src/test/java/objectRepo/TC_006.java
+++ com.crm.demo/src/test/java/objectRepo/TC_006.java
@@ -2,9 +2,6 @@ package objectRepo;
 
 import org.testng.annotations.Test;
 
-import PomClassesRepo.ContactsPage;
-import PomClassesRepo.CreatingNewContactPage;
-import PomClassesRepo.HomePage;
 import genericUtility.BaseClass;
 import genericUtility.ExcelUtility;
 
@@ -15,21 +12,21 @@ public class TC_006 extends BaseClass
 	{
 		System.out.println("Test Exceution Started");
 		HomePage hp=new HomePage(driver);
-		hp.clickOnContactsMenu();
+		hp.clickOnContactMenuBtn();
 	
 		ContactsPage cp=new ContactsPage(driver);
-		cp.clickOnCreateContactButton();
+		cp.clickOnCreateContactPlusIcon();
 	
 		ExcelUtility eUtil=new ExcelUtility();
-		String fName=eUtil.getDataFromExcel("Contacts", 8, 1);
-		String lName=eUtil.getDataFromExcel("Contacts", 8, 2);
-		String title=eUtil.getDataFromExcel("Contacts", 8, 3);
-		String email=eUtil.getDataFromExcel("Contacts", 8, 4);
-		String mailingCity=eUtil.getDataFromExcel("Contacts", 8, 5);
-		String mailingState=eUtil.getDataFromExcel("Contacts", 8, 6);
+		String fName=eUtil.fetchingDataFromExcelFile("Contacts", 8, 1);
+		String lName=eUtil.fetchingDataFromExcelFile("Contacts", 8, 2);
+		String title=eUtil.fetchingDataFromExcelFile("Contacts", 8, 3);
+		String email=eUtil.fetchingDataFromExcelFile("Contacts", 8, 4);
+		String mailingCity=eUtil.fetchingDataFromExcelFile("Contacts", 8, 5);
+		String mailingState=eUtil.fetchingDataFromExcelFile("Contacts", 8, 6);
 		
-		CreatingNewContactPage cncp=new CreatingNewContactPage(driver);
-		cncp.createNewContacts(fName, lName, title, email, mailingCity, mailingState);
+		CreateContactsPage cncp=new CreateContactsPage(driver);
+		cncp.createContactOperation(fName, lName, title, email, mailingCity, mailingState);
 		System.out.println("Test Execution Ended");
 	}
 
