37ec2ff6ecd0aa851e27dfeaed090bab66f9aad2
Sumit021995 <kaku.saurav@gmail.com>
2025-02-09 22:41:26 +0530

    new commit

--- com.crm.demo/src/main/java/genericUtility/BaseClass.java
+++ com.crm.demo/src/main/java/genericUtility/BaseClass.java
@@ -1,11 +1,9 @@
 package genericUtility;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -18,65 +16,84 @@ import org.testng.annotations.Parameters;
 import objectRepo.HomePage;
 import objectRepo.LoginPage;
 
+public class BaseClass {
 
-public class BaseClass 
-{
 	public WebDriver driver;
+	public static WebDriver sDriver;		//only for listeners
+	public SeleniumUtility sUtil=new SeleniumUtility();
 	public PropertiesUtility pUtil=new PropertiesUtility();
 	public ExcelUtility eUtil=new ExcelUtility();
-	public SeleniumUtility sUtil=new SeleniumUtility();
+	public DatabaseUtility dbUtil=new DatabaseUtility();
 	
-	@BeforeSuite
-	public void dbConnection()
+	@BeforeSuite(alwaysRun = true)
+	public void dbConnection() throws Exception
 	{
-		System.out.println("Database connected");
+		String dbURL=pUtil.getDataFromPropertiesFile("DBURL");
+		String dbUN=pUtil.getDataFromPropertiesFile("DBUN");
+		String dbPWD=pUtil.getDataFromPropertiesFile("DBPWD");
+		dbUtil.connectToDatabase(dbURL, dbUN, dbPWD);
+		System.out.println("DB connection created");
 	}
+	
 	@Parameters("browser")
-	@BeforeClass(alwaysRun=true)
-	public void launchBrowser(@Optional("chrome") String bname) throws Exception
+	@BeforeClass(alwaysRun = true)
+	public void launchBrowser(@Optional("chrome") String BROWSER) throws Exception
 	{
-		if(bname.equalsIgnoreCase("chrome"))
+		if(BROWSER.equals("chrome"))
+		{
 			driver=new ChromeDriver();
-		else if(bname.equalsIgnoreCase("firefox"))
-			driver=new FirefoxDriver();
-		else if(bname.equalsIgnoreCase("edge"))
+		}
+		else if(BROWSER.equals("edge"))
+		{
 			driver=new EdgeDriver();
-		else if(bname.equalsIgnoreCase("safari"))
-			driver=new SafariDriver();
+		}
+		else if(BROWSER.equals("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
 		else
-			driver = new ChromeDriver();
-			
+		{
+			driver=new ChromeDriver();
+		}
+		sDriver=driver;		//this sDriver is initialized and will be used in Listeners
+		sUtil.implicitWait(driver, 15);
 		sUtil.maximizeWindow(driver);
-		sUtil.implicitWait(driver,15);
-		String URl=pUtil.getDataFromPropertiesFile("url");
-		sUtil.accesToApplication(driver, URl);
-		System.out.println("application accessed");
+		String URL=pUtil.getDataFromPropertiesFile("url");
+		sUtil.navigateToUrl(driver, URL);
+		System.out.println("Browser launched successfully");
 	}
-	@BeforeMethod
+	
+	@BeforeMethod(alwaysRun = true)
 	public void loginOperation() throws Exception
 	{
-		String UN=pUtil.getDataFromPropertiesFile("username");
-		String PWD=pUtil.getDataFromPropertiesFile("password");
+//		String UN=pUtil.getDataFromPropertiesFile("username");
+//		String PWD=pUtil.getDataFromPropertiesFile("password");
+		String UN=dbUtil.fetchDataFromTable("User1", "username");
+		String PWD=dbUtil.fetchDataFromTable("User1", "password");
 		LoginPage lp=new LoginPage(driver);
 		lp.loginToApplication(UN, PWD);
-		System.out.println("login done");
+		System.out.println("Login done successfully");
 	}
-	@AfterMethod
-	public void logOutoperation()  throws Exception
+	
+	@AfterMethod(alwaysRun = true)
+	public void logoutOperation()
 	{
 		HomePage hp=new HomePage(driver);
 		hp.signOutOperation(driver);
-		System.out.println("sign out done");
+		System.out.println("Sign out done successfully");
 	}
-	@AfterClass
-	public void closeBrowser() throws Exception
+	
+	@AfterClass(alwaysRun = true)
+	public void closeBrowser()
 	{
 		driver.quit();
-		System.out.println("Browser Closed");
+		System.out.println("Browser closed successfully");
 	}
-	@AfterSuite
-	public void dbConnectionClose() 
+	
+	@AfterSuite(alwaysRun = true)
+	public void closeDBConnection() throws Exception
 	{
-		System.out.println("Database disconnected");
+		dbUtil.closeDBConnection();
+		System.out.println("DB connection closed");
 	}
 }
