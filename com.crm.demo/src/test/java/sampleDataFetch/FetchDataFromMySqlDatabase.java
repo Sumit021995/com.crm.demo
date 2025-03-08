36cda80c3cb3caf0ab83031c664b7169319763de
Sumit021995 <kaku.saurav@gmail.com>
2025-03-09 00:50:22 +0530

    new commit

--- com.crm.demo/src/test/java/sampleDataFetch/FetchDataFromMySqlDatabase.java
+++ com.crm.demo/src/test/java/sampleDataFetch/FetchDataFromMySqlDatabase.java
@@ -1,12 +1,14 @@
 package sampleDataFetch;
 
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.mysql.cj.jdbc.Driver;
 
 public class FetchDataFromMySqlDatabase {
 	public static void main(String[] args) throws SQLException {
-		Driver driverRef = new Driver();
+		PropertiesUtility pUtility = new PropertiesUtility();
+		DriverManager.getConnection(null, null, null)
 		
 	}
 }
